5e3ab34663fe720241d2cfda2586eb9fb165a0ee
Finan Meharena <finanmeharenakidane@gmail.com>
2024-09-13 18:16:50 -0700

    registered models

--- leads/models.py
+++ leads/models.py
@@ -9,12 +9,18 @@ class User(AbstractUser): # inheriting fields lile first_name, last_name, email,
 class Agent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     
+    def __str__(self):
+        return self.user.username
+    
 class Lead(models.Model):
     
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     age = models.IntegerField(default=0)
     agent = models.ForeignKey("Agent", on_delete=models.CASCADE, null=True, default=1)
+    
+    def __str__(self):
+        return self.first_name
     
     
     # SOURCE_CHOICES = (
