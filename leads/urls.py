531984ebd097d10797772c9d2a05b1194abe4398
Finan Meharena <finanmeharenakidane@gmail.com>
2024-09-16 10:03:52 -0700

    added lead create form

--- leads/urls.py
+++ leads/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import lead_detail, lead_list
+from .views import lead_detail, lead_list, lead_create
 
 app_name = "leads"
 
 urlpatterns = [
     path("", lead_list),
-    path("<pk>/", lead_detail)
+    path("create/", lead_create),
+    path("<pk>/", lead_detail),
 ]
--- leads/views.py
+++ leads/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-from .models import Lead
+from django.shortcuts import render, redirect
+from .models import Lead, Agent
+from .forms import LeadForm
 
 
 def lead_list(request):
@@ -17,4 +18,25 @@ def lead_detail(request,pk):
         "lead" : lead
     }
     return render(request, 'leads/lead_detail.html', context = context)
-    
+
+def lead_create(request):
+    form = LeadForm()
+    print(request.POST)
+    if request.method == "POST":
+        form = LeadForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            age = form.cleaned_data['age']
+            agent = Agent.objects.first()
+            Lead.objects.create(
+                first_name = first_name,
+                last_name = last_name,
+                age = age,
+                agent = agent,
+            )
+            return redirect("/leads")
+    context = {
+        "form" : form,
+    }
+    return render(request, "leads/lead_create.html", context)
