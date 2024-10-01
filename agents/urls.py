30795af0c2dd9a977e666b211c8f74701b7b20d0
Finan Meharena <finanmeharenakidane@gmail.com>
2024-10-01 10:12:02 -0700

    created lead-create route, view and form to create agent

--- agents/urls.py
+++ agents/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from agents.views import AgentListView
+from agents.views import AgentListView, AgentCreateView
 
 app_name = 'agents'
 
 urlpatterns = [
-    path("", AgentListView.as_view(), name="agents-list")
+    path("", AgentListView.as_view(), name="agents-list"),
+    path("agent-create/",AgentCreateView.as_view(), name="agent-create"),
+    
 ]
--- agents/views.py
+++ agents/views.py
@@ -1,19 +1,32 @@
 from typing import Any
 from django.db.models.query import QuerySet
-from django.shortcuts import render
+from django.shortcuts import reverse
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 from leads.models import Agent
+from .forms import AgentModelForm
 
 
-class AgentListView( LoginRequiredMixin, generic.ListView):
+class AgentListView(LoginRequiredMixin, generic.ListView):
     template_name = "agents/agent_list.html"
     context_object_name = 'agents'
-    
+
     def get_queryset(self) -> QuerySet[Any]:
         return Agent.objects.all()
     
+
 class AgentCreateView(LoginRequiredMixin, generic.CreateView):
     template_name = 'agents/agent_create.html'
+    form_class = AgentModelForm
     
+    def get_success_url(self):
+        return reverse("agents:agents-list")
+    
+    def form_valid(self, form):
+        agent = form.save(commit=False)
+        agent.organization = self.request.user.userprofile
+        agent.save()
+        return super(AgentCreateView, self).form_valid(form)
+    
+
 # Create your views here.
